433f72dcf8cc101ab24a506b16b3f8573cf864b9
Jori Villiger <jori.villiger@swisscom.com>
2024-03-27 15:02:49 +0100

    angebote

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import {Routes} from '@angular/router';
 import {HomepageComponent} from "./homepage/homepage.component";
 import {ContactComponent} from "./contact/contact.component";
+import { AngeboteComponent } from './angebote/angebote.component';
 
 export const routes: Routes = [
   {path: '', component: HomepageComponent},
-  {path: 'contact', component: ContactComponent}
+  {path: 'contact', component: ContactComponent},
+  {path: 'angebote', component: AngeboteComponent}
 ];
